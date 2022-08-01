8bd2168535b16ad7c51b4fc1b4d48b2aae944ba2
Alex Bruno de Oliveira <alexbrunoliveira@gmail.com>
2022-07-31 22:24:34 -0300

    desafios TypeScript

---
+++ desafio2.ts
@@ -0,0 +1,16 @@
+// iniciando variável de qualquer tipo
+let cpf: any;
+
+// tipo string
+cpf = "12366548792";
+// tipo boolean
+cpf = true;
+
+// tornando a variável do tipo numerica
+cpf = 10;
+
+function mostraCPF(cpf: string) {
+    console.log(cpf)
+}
+
+mostraCPF(cpf); 
